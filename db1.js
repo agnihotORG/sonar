44860eecc502ca6fbe17655a51eec402eb6c8bd1
agnihot <abhay.agnihotri3@t-mobile.com>
2017-01-19 20:02:25 -0600

    Update db1.js

--- db1.js
+++ db1.js
@@ -13,10 +13,4 @@
         document.write(rs.fields(4) + "<br/>");
         rs.movenext();
     				}
-    var a = NaN;
 
-if (a === NaN) {  // Noncompliant; always false
-  console.log("a is not a number");  // this is dead code
-}
-if (a !== NaN) { // Noncompliant; always true
-  console.log("a is not NaN"); // this statement is not necessarily true
